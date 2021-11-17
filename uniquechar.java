fe946828145ad36cf91f18bb06b6272a19ab979c
Jeet Undhad <jundhad@uwo.ca>
2021-11-16 19:39:49 -0500

    leetcode 387

---
+++ uniquechar.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class uniquechar {
+    public static void main(String[] args){
+        System.out.println(solution("abcded"));
+    }
+
+    public static int solution(String s) {
+        HashMap<Character, Integer> map = new HashMap<Character , Integer>();
+        for(int i = 0; i< s.length(); i++){
+            if(map.get(s.charAt(i))==null){
+                map.put(s.charAt(i), 1);
+            } else {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            }
+        }
+        System.out.println(map.toString());
+        for(int i = 0; i< s.length(); i++){
+            if(map.get(s.charAt(i))=1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
