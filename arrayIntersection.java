9c9391946996eb8f6f0bff4b5c1e3a75d52eee36
Jeet Undhad <jrundhad@gmail.com>
2021-11-18 18:01:44 -0500

    leetcode 349

---
+++ arrayIntersection.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class arrayIntersection {
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(solution(new int[] {1,2,2,1}, new int[] {2,2})));
+    }
+
+    public static int[] solution(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        HashSet<Integer> ans = new HashSet<Integer>();
+        for(int i=0; i<nums1.length; i++){
+            set.add(nums1[i]);
+        }
+        for(int i=0; i<nums2.length; i++){
+            if(set.contains(nums2[i])){
+                ans.add(nums2[i]);
+            }
+        }
+        return ans.stream().mapToInt(i -> i).toArray();
+        
+    }
+}
