0e8d8387befb7b879f5eedfb6753ba6d41ffd5e4
Jeet Undhad <jrundhad@gmail.com>
2021-12-03 14:14:57 -0500

    post order traversal leetcode 145

---
+++ dfsPostOrder.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class dfsPostOrder {
+    List<Integer> list = new ArrayList<Integer>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if(root!=null){
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            list.add(root.val);
+            
+        }
+        return list;
+    }
+}
