aa03197a0bacffb61e683dec83cfdb78bb7ddfbe
Jeet Undhad <jrundhad@gmail.com>
2021-12-17 22:14:55 -0700

    leetcode 876

---
+++ middleList.java
@@ -0,0 +1,18 @@
+public class middleList {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp = head;
+        int count = 0;
+
+        while(temp!=null){
+            count ++;
+            temp = temp.next;
+        }
+
+        for(int i =0; i<(count/2); i ++){
+            head = head.next;
+        }
+
+        return head;
+
+    }
+}
