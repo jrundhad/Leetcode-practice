5aa3925640266e64fc870cbc7709556526bd153c
Jeet Undhad <jrundhad@gmail.com>
2021-12-18 10:52:21 -0700

    160

---
+++ intersectionlinked.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class intersectionlinked {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        HashSet<ListNode> set = new HashSet<>();
+        while(headA!=null){
+            set.add(headA);
+            headA = headA.next;
+        }
+        while(headB!=null){
+            if(set.contains(headB)){
+                return headB;
+            }
+            headB = headB.next;
+
+        }
+        return null;
+    }
+}
