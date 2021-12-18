711300ef10293c560011e582b84b5b716771dcc1
Jeet Undhad <jrundhad@gmail.com>
2021-12-17 22:32:10 -0700

    141

--- linkedcycle.java
+++ linkedcycle.java
@@ -3,9 +3,10 @@ import java.util.*;
 public class linkedcycle {
     public boolean hasCycle(ListNode head) {
         HashSet<ListNode> set = new HashSet<ListNode>();
+        set.add(head);
         while(head!=null){
-            if(!set.contains(head)){
-                set.add(head);
+            if(!set.contains(head.next)){
+                set.add(head.next);
                 head= head.next;
             }else{
                 return true;
