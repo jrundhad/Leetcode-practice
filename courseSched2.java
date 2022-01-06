0e72e0107995da71329a8deb77553d1c38a6e5a7
Jeet Undhad <jrundhad@gmail.com>
2022-01-06 14:52:24 -0700

    course sched 2

--- courseSched2.java
+++ courseSched2.java
@@ -4,22 +4,17 @@ public class courseSched2 {
         ArrayList<Integer>[] adjlist  = createGraph(prerequisites, numCourses);
         int[] visited = new int[numCourses];
         LinkedList<Integer> result = new LinkedList<Integer>();
-        boolean x = false;
 
         for(int i=0;i<visited.length;i++){
-            x = dfs(adjlist,visited, i, result);
-            if(x==false){
-                break;
+            if(!dfs(adjlist,visited, i, result)){
+                return new int[]{};
             }
         }
-        System.out.println(x);
-        if(x){
-            for(int i=0; i<result.size(); i++){
-            visited[i]= result.get(i);
-            }
-            return visited;
+
+        for(int i=0; i<result.size(); i++){
+        visited[i]= result.get(i);
         }
-        return new int[]{};
+        return visited;
     }
 
     private boolean dfs(ArrayList<Integer>[] adjlist, int[] visited, int i, LinkedList<Integer> result) {
