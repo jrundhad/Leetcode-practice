823ec4fb80d7d08f67c8b50ad167598aa30e10d3
Jeet Undhad <jrundhad@gmail.com>
2022-01-07 20:50:45 -0500

    longest sub string

--- longestSubstring.java
+++ longestSubstring.java
@@ -1,22 +1,24 @@
 public class longestSubstring {
-    int left =0;
-    int right = 0;
-    int max =0;
-    if(s.length()==0 || s==null){
-        return 0;
-    }
-    HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-    while(right<s.length()){
-        if(!map.containsKey(s.charAt(right))){
-            map.put(s.charAt(right), right);
-        } else if(map.containsKey(s.charAt(right))){
-            left = Math.max(left,map.get(s.charAt(right))+1);
-            map.put(s.charAt(right), right);
+    public int lengthOfLongestSubstring(String s) {
+        int left =0;
+        int right = 0;
+        int max =0;
+        if(s.length()==0 || s==null){
+            return 0;
         }
-        max = Math.max(right-left+1, max);
-        
-        right ++;
-        
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        while(right<s.length()){
+            if(!map.containsKey(s.charAt(right))){
+                map.put(s.charAt(right), right);
+            } else if(map.containsKey(s.charAt(right))){
+                left = Math.max(left,map.get(s.charAt(right))+1);
+                map.put(s.charAt(right), right);
+            }
+            max = Math.max(right-left+1, max);
+            
+            right ++;
+            
+        }
+        return max;
     }
-    return max;
 }
