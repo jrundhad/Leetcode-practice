d0d5792c36c05b6bd8e8bb2f8b3d1fb3b0571271
Jeet Undhad <jrundhad@gmail.com>
2022-02-02 14:30:32 -0500

    optimized sol

--- threeSum.java
+++ threeSum.java
@@ -8,28 +8,33 @@ public class threeSum {
     }
 
     public static List<List<Integer>> solution(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(nums.length==0|| nums==null){
+            return result;
+        }
         Arrays.sort(nums);
-        Set<List<Integer>> result=new HashSet<>();
-        for(int i=0; i<nums.length; i++){
-            int num = nums[i];
-            HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-            for(int j=i+1; j<nums.length; j++){
-                //if(j==i)j++;
-                //if(j==nums.length) break;
-                int key = 0 - nums[i] - nums[j];
-                if(map.containsKey(key)){
-                    List<Integer> list= new ArrayList<Integer>(); 
-                    list.add(num);
-                    list.add(nums[j]);
-                    list.add(key);
-                    //Collections.sort(list);
-                    result.add(list);
-                } else {
-                    map.put(nums[j], j);
+        for(int i =0; i<nums.length-2; i++){
+            if(i==0 || (i>0 && nums[i]!= nums[i-1])){
+                int low = i+1;
+                int high = nums.length-1;
+                int sum = 0-nums[i];
+
+                while(low < high){
+                    if(nums[low]+nums[high]==sum){
+                        result.add(Arrays.asList(nums[i],nums[low],nums[high]));
+                        while(low < high && nums[low]==nums[low+1]) low++;
+                        while(low < high && nums[high]== nums[high-1]) high--;
+                        low++;
+                        high--;
+                    } else if(nums[low]+nums[high]>sum){
+                        high--;
+                    } else{
+                        low ++;
+                    }
                 }
             }
         }
-        return new ArrayList<>(result);
+        return result;
     }
 
 }
