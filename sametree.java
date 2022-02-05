72c28500b737fff8fe6f7472fc52ab186f50591a
Jeet Undhad <jrundhad@gmail.com>
2022-02-05 17:58:48 -0500

    same tree o(n)

--- sametree.java
+++ sametree.java
@@ -2,23 +2,12 @@ import java.util.*;
 
 public class sametree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return traversal(p, q);
-    }
-
-    public boolean traversal(TreeNode root, TreeNode root2){
-        boolean x= true;
-        boolean y= true;
-        if(root!=null && root2!=null && root2.val==root.val){
-            x = traversal(root.left, root2.left);
-            y = traversal(root.right, root2.right);    
+        if(p==null && q==null){
+            return true;
         }
-        else if (root!=root2){
+        if((p!=null && q!=null && p.val != q.val) || (p!=null && q==null) || (q!=null && p==null)){
             return false;
         }
-        if(x==y && x==true){
-                return x;
-            }else{
-                return false;
-            } 
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
