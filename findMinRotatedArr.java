7cb538b51061ce014deac1c4f56337e38e8e8c90
Jeet Undhad <jrundhad@gmail.com>
2022-02-18 23:20:55 -0500

    find min in rotated arr

---
+++ findMinRotatedArr.java
@@ -0,0 +1,19 @@
+public class findMinRotatedArr {
+    public int findMin(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+        int left =0;
+        int right = nums.length-1;
+        while(left<right){
+            int midpoint = left + (right-left)/2;
+            if(nums[midpoint]>nums[right]){
+                left = midpoint +1;
+            }
+            else {
+                right = midpoint;
+            }
+        }
+        return nums[left];
+    }
+}         
