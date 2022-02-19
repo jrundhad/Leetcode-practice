09259f9d4683ae3aa03ad68952a623e5857939f1
Jeet Undhad <jrundhad@gmail.com>
2022-02-18 23:23:42 -0500

    O(n)

--- findMinRotatedArr.java
+++ findMinRotatedArr.java
@@ -5,12 +5,12 @@ public class findMinRotatedArr {
         }
         int left =0;
         int right = nums.length-1;
-        while(left<right){
-            int midpoint = left + (right-left)/2;
+        while(left < right){
+            int midpoint = left +(right -left)/2;
             if(nums[midpoint]>nums[right]){
                 left = midpoint +1;
             }
-            else {
+            else{
                 right = midpoint;
             }
         }
